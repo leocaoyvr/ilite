257440f88f0bc470d5fcd8574f7c62de00cbb5f6
vivianalive <vivi@ilite.me>
2015-07-18 20:52:29 -0700

    removed social links

--- sidebar.php
+++ sidebar.php
@@ -1,15 +1,4 @@
 <div id="sidebar">
-	<!--
- 	<div id="socials" class="widget-area widget-sidebar">
-		<h3>Follow Me!</h3>
-		<a href="http://ilite.me/feed/" target="_blank" title="Subscribe to I LITE" class="socialicon rss">rss</a>
-		<a href="mailto://vivianalive.hotmail.com" title="Write email to me" class="socialicon mail">email</a>
-		<a href="skype:vivianalive?call" title="Call me via Skype" class="socialicon skype">skype</a>
-		<a href="https://plus.google.com/116704076391108813160/" target="_blank"  title="Visit my Google+ page" class="socialicon gp">google+</a>
-		<a href="http://twitter.com/vivianalive" target="_blank" title="Follow my Twitter" class="socialicon twitter">twitter</a>
-		<a href="#top" id="mtop">TOP ↑</a>
-	</div>
-	-->
 
 	<?php if ( ! dynamic_sidebar( 'Sidebar' )) : ?>
 	<ul>
