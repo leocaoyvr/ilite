bfdf36fbb08cf5e4522ee75bfca13acc2c69a182
Leo Cao <leo@ilite.me>
2017-06-30 23:28:29 -0700

    add google analytics tracking snippet

--- footer.php
+++ footer.php
@@ -8,5 +8,16 @@
 		</footer>
 	</div><!--#footer-->
 <?php wp_footer(); /* this is used by many Wordpress features and plugins to work proporly */ ?>
+
+<script><!-- Google Analytics begin -->
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-23230199-3', 'auto');
+  ga('send', 'pageview');
+</script><!-- Google Analytics end -->
+
 </body>
 </html>
