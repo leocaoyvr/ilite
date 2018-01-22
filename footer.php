02bf3a3e85d84f823fe674954ee5774171e71ec0
Leo Cao <leo@ilite.me>
2018-01-22 00:54:03 -0800

    update google tracking code

--- footer.php
+++ footer.php
@@ -9,15 +9,15 @@
 	</div><!--#footer-->
 <?php wp_footer(); /* this is used by many Wordpress features and plugins to work proporly */ ?>
 
-<script><!-- Google Analytics begin -->
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23230199-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  ga('create', 'UA-23230199-3', 'auto');
-  ga('send', 'pageview');
-</script><!-- Google Analytics end -->
+  gtag('config', 'UA-23230199-3');
+</script>
 
 </body>
 </html>
